9bc3befdbc78e6a1e0dc687fd63e71a7713e95ca
Darren Behan <darrenbehan@hotmail.com>
2020-08-25 11:55:42 +1000

    adding border-color to bottom of nav, updated about section and introduced AOS library for animations

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,114 +1,54 @@
-$(function () {
+// "document.ready" makes sure that our JavaScript doesn't get run until the HTML document is finished loading.
+$(document).ready(() => {
+  // Initializing AOS => https://michalsnik.github.io/aos/
+  AOS.init({
+    duration: 1200,
+  });
+
+  // When you click the 'View my work' button, the viewport scrolls down to the navigation menu and stops once that meets the top of the viewport
   $(".btn-home").click(function() {
     $('html, body').animate({
         scrollTop: $(".navbar").offset().top
     }, 1000);
   });
 
-  $(window).on("load", function () {
-    $(window)
-      .scroll(function () {
-        var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-        $(".fade").each(function () {
-          /* Check the location of each desired element */
-          var objectBottom = $(this).offset().top + $(this).outerHeight();
+  // This function will run when the window loads
+  $(window).on("load", () => {
+    // We're creating two <div> elements to store the About heading which animates on scroll which is controlled through AOS
+    // The animations are introduced by adding classes to the elements
+    const $aboutDiv = $(".about");
+    const $h2 = $("<h2>").text("About").attr("data-aos", "fade-right");
+    $aboutDiv.append($h2);
 
-          /* If the element is completely within bounds of the window, fade it in */
-          if (objectBottom < windowBottom) {
-            //object comes into view (scrolling down)
-            if ($(this).css("opacity") == 0) {
-              $(this).fadeTo(500, 1);
-            }
-          } else {
-            //object goes out of view (scrolling up)
-            if ($(this).css("opacity") == 1) {
-              $(this).fadeTo(500, 0);
-            }
-          }
-        });
-      })
-      .scroll(); //invoke scroll-handler on page-load
+    const $headerBarDiv = $("<div>").attr("class", "header-bar").attr("data-aos", "fade-left");
+    $aboutDiv.append($headerBarDiv);
   });
+  
 
-  $(window).on("load", function () {
-    $("#skills").hide();
-    $("#career").hide();
-    $("#portfolio").hide();
-    $("#education").hide();
-    $("#contact").hide();
-  });
+  // $(window).on("load", function () {
+  //   $(window)
+  //     .scroll(function () {
+  //       var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+  //       $(".fade").each(function () {
+  //         /* Check the location of each desired element */
+  //         var objectBottom = $(this).offset().top + $(this).outerHeight();
 
-  $("#nav-about").on("click", function () {
-    $("#skills").hide();
-    $("#career").hide();
-    $("#portfolio").hide();
-    $("#education").hide();
-    $("#contact").hide();
-    $("#about").show();
-  });
-
-  $("#nav-skills").on("click", function () {
-    $("#about").hide();
-    $("#career").hide();
-    $("#portfolio").hide();
-    $("#education").hide();
-    $("#contact").hide();
-    $("#skills").show();
-  });
-
-  $("#nav-career").on("click", function () {
-    $("#about").hide();
-    $("#skills").hide();
-    $("#portfolio").hide();
-    $("#education").hide();
-    $("#contact").hide();
-    $("#career").show();
-  });
-
-  $("#nav-portfolio").on("click", function () {
-    $("#about").hide();
-    $("#skills").hide();
-    $("#career").hide();
-    $("#education").hide();
-    $("#contact").hide();
-    $("#portfolio").show();
-  });
-
-  $("#nav-education").on("click", function () {
-    $("#about").hide();
-    $("#skills").hide();
-    $("#career").hide();
-    $("#portfolio").hide();
-    $("#contact").hide();
-    $("#education").show();
-  });
-
-  $("#nav-contact").on("click", function () {
-    $("#about").hide();
-    $("#skills").hide();
-    $("#career").hide();
-    $("#portfolio").hide();
-    $("#education").hide();
-    $("#contact").show();
-  });
-
-  $("#nav-icon-contact").on("click", function () {
-    $("#about").hide();
-    $("#skills").hide();
-    $("#career").hide();
-    $("#portfolio").hide();
-    $("#education").hide();
-    $("#contact").show();
-  });
-
-  $(".btn-success").on("click", function () {
-    $("#about").hide();
-    $("#skills").hide();
-    $("#career").hide();
-    $("#portfolio").hide();
-    $("#education").hide();
-    $("#contact").show();
-  });
+  //         /* If the element is completely within bounds of the window, fade it in */
+  //         if (objectBottom < windowBottom) {
+  //           //object comes into view (scrolling down)
+  //           if ($(this).css("opacity") == 0) {
+  //             $(this).fadeTo(500, 1);
+  //           }
+  //         } else {
+  //           //object goes out of view (scrolling up)
+  //           if ($(this).css("opacity") == 1) {
+  //             $(this).fadeTo(500, 0);
+  //           }
+  //         }
+  //       });
+  //     })
+  //     .scroll(); //invoke scroll-handler on page-load
+  // });
 
   // charts.js
   var ctx = $("#skills-chart");
