b51ba6d9b3c3f139e6fe7fc0654c65cedcb3a2a3
Darren Behan <darrenbehan@hotmail.com>
2020-08-25 20:15:44 +1000

    adjusted styling for img, text, button, refactored css sheet, dynamically appending elements

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,10 +6,13 @@ $(document).ready(() => {
   });
 
   // When you click the 'View my work' button, the viewport scrolls down to the navigation menu and stops once that meets the top of the viewport
-  $(".btn-home").click(function() {
-    $('html, body').animate({
-        scrollTop: $(".navbar").offset().top
-    }, 1000);
+  $(".btn-home").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $(".navbar").offset().top,
+      },
+      1000
+    );
   });
 
   // This function will run when the window loads
@@ -18,59 +21,72 @@ $(document).ready(() => {
   });
 
   const aboutSection = () => {
-    // We're dynamically creating all elements for the About section with certain elements animating on scroll which is controlled through AOS
+    // We're dynamically creating all elements for the About section with each element animating on scroll which is controlled through the AOS library
     // The animations are introduced by adding classes to the elements
     const $aboutDiv = $(".about");
     const $h2 = $("<h2>").text("About").attr("data-aos", "fade-right");
     $aboutDiv.append($h2);
 
-    const $headerBarDiv = $("<div>").attr("class", "header-bar").attr("data-aos", "fade-left");
+    const $headerBarDiv = $("<div>")
+      .attr("class", "header-bar")
+      .attr("data-aos", "fade-left");
     $aboutDiv.append($headerBarDiv);
 
     const $aboutRow = $("<div>").attr("class", "row");
     $aboutDiv.append($aboutRow);
 
-    const $bioDivLeft = $("<div>").attr("class", "col-md-4 bio-text-left").attr("data-aos", "fade-right");
-    const $bioTextLeft = $("<p>").text("Full Stack Web Developer utilising an extensive customer service and finance background to build complex web apps with a seamless user experience. Currently enrolled in Monash Universities Full Stack Web Development Bootcamp developing where I’m developing technical skills in CSS, JavaScript, React.js Node.js, API, MongoDB and responsive web design.");
+    const $bioDivLeft = $("<div>")
+      .attr("class", "col-md-4 bio-text-left")
+      .attr("data-aos", "fade-right");
+    const $bioTextLeft = $("<p>").text(
+      "Full Stack Web Developer utilising an extensive customer service and finance background to build complex web apps with a seamless user experience."
+    );
     $aboutRow.append($bioDivLeft);
     $bioDivLeft.append($bioTextLeft);
 
-    const $bioDivImg = $("<div>").attr("class", "col-md-4 profile-pic").attr("data-aos", "fade-up");
+    const $bioDivImg = $("<div>")
+      .attr("class", "col-md-4 profile-pic")
+      .attr("data-aos", "fade-up");
     const $bioImg = $("<img>").attr("src", "./assets/img/profile-pic.png");
     $aboutRow.append($bioDivImg);
     $bioDivImg.append($bioImg);
 
-    const $bioDivRight = $("<div>").attr("class", "col-md-4 bio-text-left").attr("data-aos", "fade-left");
-    const $bioTextRight = $("<p>").text("My known hard work ethic coupled with my passion for developing responsive, clean web apps has allowed me the opportunity to work on developing and deploying custom built NetSuite scripts to automate processes which drive a better user experience for my stakeholders allowing them to focus on their core role tasks over manual tasks. I’m thrilled to leverage my newly developed skills to continue to deliver automated solutions and better user experiences.");
+    const $bioDivButton = $("<div>").attr(
+      "class",
+      "d-flex justify-content-center col-md-12 button-sm"
+    );
+    const $bioButtonCv = $("<button>")
+      .attr("type", "button")
+      .attr("class", "btn btn-outline-primary d-flex align-items-center button-md");
+    const $bioLinkCv = $("<a>")
+      .attr("class", "button-md")
+      .attr("href", "./assets/img/cv.pdf")
+      .text("Download CV");
+    $bioDivImg.append($bioDivButton);
+    $bioDivButton.append($bioButtonCv);
+    $bioButtonCv.append($bioLinkCv);
+
+    const $bioDivRight = $("<div>")
+      .attr("class", "col-md-4 bio-text-right")
+      .attr("data-aos", "fade-left");
+    const $bioTextRight = $("<p>").text(
+      "Currently enrolled in Monash Universities Full Stack Web Development Bootcamp where I’m developing technical skills in CSS, JavaScript, React.js Node.js, API, MongoDB and responsive web design."
+    );
     $aboutRow.append($bioDivRight);
     $bioDivRight.append($bioTextRight);
-  }
-  
+  };
 
-  // $(window).on("load", function () {
-  //   $(window)
-  //     .scroll(function () {
-  //       var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-  //       $(".fade").each(function () {
-  //         /* Check the location of each desired element */
-  //         var objectBottom = $(this).offset().top + $(this).outerHeight();
-
-  //         /* If the element is completely within bounds of the window, fade it in */
-  //         if (objectBottom < windowBottom) {
-  //           //object comes into view (scrolling down)
-  //           if ($(this).css("opacity") == 0) {
-  //             $(this).fadeTo(500, 1);
-  //           }
-  //         } else {
-  //           //object goes out of view (scrolling up)
-  //           if ($(this).css("opacity") == 1) {
-  //             $(this).fadeTo(500, 0);
-  //           }
-  //         }
-  //       });
-  //     })
-  //     .scroll(); //invoke scroll-handler on page-load
-  // });
+  $(window).resize(() => {
+    if ($(window).width() < 500) {
+      $(".profile-pic").addClass("order-1");
+      $(".bio-text-left").addClass("order-2");
+      $(".bio-text-right").addClass("order-3");
+    } else {
+      $(".profile-pic").removeClass("order-1");
+      $(".bio-text-left").removeClass("order-2");
+      $(".bio-text-right").removeClass("order-3");
+    }
+  });
 
   // charts.js
   var ctx = $("#skills-chart");
