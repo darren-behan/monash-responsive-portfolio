575757f9d774ff57a81fa9cb6f87a0bfc2aeb292
Darren Behan <darrenbehan@hotmail.com>
2020-09-21 18:32:59 +1000

    adding portfolio component

---
+++ src/components/SectionPortfolio/index.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import './index.css';
+import { Container, Row } from 'react-bootstrap';
+import Portfolio from '../Portfolio';
+import PortfolioArray from '../../portfolio'
+
+function PortfolioSection() {
+  // Setting this.state.portfolio to the portfolio json array
+  const [portfolioArray, setPortfolioArray] = useState(PortfolioArray);
+
+  return (
+    <section id="section-portfolio">
+      <Container>
+        <Row>
+          <div className="portfolio col-md-12 flex">
+            <h2 data-aos="fade-right">Portfolio</h2>
+            <div className="header-bar" data-aos="fade-left" />
+            <Row id="portfolio-row">
+              {portfolioArray.map(portfolio => (
+                  <Portfolio 
+                    image={portfolio.image}
+                    app={portfolio.app}
+                    name={portfolio.name}
+                    github={portfolio.github}
+                  />
+                )
+              )}
+            </Row>
+          </div>
+        </Row>
+      </Container>
+    </section>
+  )
+}
+
+export default PortfolioSection
+
+// portfolio.forEach(element => {
+//   const $portfolioRowDivEl = $("#portfolio-row");
+//   const $portfolioColDivEl = $("<div>").attr("class", "col-lg-3 col-md-4 col-sm-6 col-xs-12").attr("data-aos", "zoom-in-up");
+//   const $hoverEffectDivEl = $("<div>").attr("class", "hovereffect");
+//   const $responsiveImgEl = $("<img>").attr("class", "img-responsive").attr("alt", "").attr("src", element.image);
+//   const $overlayDivEl = $("<div>").attr("class", "overlay");
+//   const $appLinkEl = $("<a>").attr("href", element.app).attr("target", "_blank");
+//   const $pEl = $("<p>");
+//   const $repoLinkEl = $("<a>").attr("href", element.github).attr("target", "_blank");
+//   const $githubFontEl = $("<i>").attr("class", "fab fa-github");
+
+//   $portfolioRowDivEl.append($portfolioColDivEl);
+//   $portfolioColDivEl.append($hoverEffectDivEl);
+//   $hoverEffectDivEl.append($responsiveImgEl);
+//   $hoverEffectDivEl.append($overlayDivEl);
+//   $overlayDivEl.append($appLinkEl);
+//   $appLinkEl.html("<h2>" + element.name + "</h2>");
+//   $overlayDivEl.append($pEl);
+//   $pEl.append($repoLinkEl);
+//   $repoLinkEl.append($githubFontEl);
+// });
---
+++ src/portfolio.js
@@ -0,0 +1,59 @@
+import beRedy from './assets/img/be-redy.png';
+import aBetterTime from './assets/img/a-better-time.png';
+import burgerLogger from './assets/img/burger-logger.png';
+import readmeGenerator from './assets/img/readme-generator.png';
+import employeeTracker from './assets/img/employee-tracker.png';
+import weatherDashboard from './assets/img/weather-dashboard.png';
+import workDayScheduler from './assets/img/work-day-scheduler.png';
+import workoutTracker from './assets/img/workout-tracker.png';
+
+export default [
+  {
+    name: "BE-REDY",
+    image: beRedy,
+    app: "https://be-redy.herokuapp.com",
+    github: "https://github.com/darren-behan/be-redy"
+  },
+  {
+    name: "A BETTER TIME",
+    image: aBetterTime,
+    app: "https://darren-behan.github.io/a-better-time/",
+    github: "https://github.com/darren-behan/a-better-time"
+  },
+  {
+    name: "BURGER LOGGER",
+    image: burgerLogger,
+    app: "https://enigmatic-cove-34274.herokuapp.com",
+    github: "https://github.com/darren-behan/burger-logger"
+  },
+  {
+    name: "README-GENERATOR",
+    image: readmeGenerator,
+    app: "https://drive.google.com/file/d/1cVIweJ_w4y9fEC4dc8TGAjuIwE_CrJGn/view",
+    github: "https://github.com/darren-behan/readme-generator"
+  },
+  {
+    name: "EMPLOYEE TRACKER",
+    image: employeeTracker,
+    app: "https://drive.google.com/file/d/1BXL7Nv1bYQR8CsNGGPSbc6TmxPr4nTQd/view",
+    github: "https://github.com/darren-behan/employee-tracker"
+  },
+  {
+    name: "WEATHER DASHBOARD",
+    image: weatherDashboard,
+    app: "https://darren-behan.github.io/weather-app/",
+    github: "https://github.com/darren-behan/weather-app"
+  },
+  {
+    name: "WORK SCHEDULER",
+    image: workDayScheduler,
+    app: "https://darren-behan.github.io/work-day-scheduler/",
+    github: "https://github.com/darren-behan/work-day-scheduler"
+  },
+  {
+    name: "WORKOUT TRACKER",
+    image: workoutTracker,
+    app: "https://track-exercises.herokuapp.com",
+    github: "https://github.com/darren-behan/workout-tracker"
+  }
+]
