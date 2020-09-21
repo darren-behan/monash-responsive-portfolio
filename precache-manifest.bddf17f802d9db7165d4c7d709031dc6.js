396d3ac9dff0636de6a2f82dbb55fcbb181179e1
Darren Behan <darrenbehan@hotmail.com>
2020-09-21 21:51:20 +1000

    Updates

---
+++ precache-manifest.bddf17f802d9db7165d4c7d709031dc6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf38caba86a8f40ca7b705a5ee440b6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "121b14a1c08282fd3c1d",
+    "url": "/static/css/2.7513ed2d.chunk.css"
+  },
+  {
+    "revision": "f62cccb7b17c0dc0a414",
+    "url": "/static/css/main.8ad032e4.chunk.css"
+  },
+  {
+    "revision": "121b14a1c08282fd3c1d",
+    "url": "/static/js/2.2668ff37.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.2668ff37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f62cccb7b17c0dc0a414",
+    "url": "/static/js/main.09fb2636.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "377f5db7924318571850e4d7b7be6de7",
+    "url": "/static/media/a-better-time.377f5db7.png"
+  },
+  {
+    "revision": "ec3600bf1c4ff23f33857a7b670ac0de",
+    "url": "/static/media/burger-logger.ec3600bf.png"
+  },
+  {
+    "revision": "4ee9ace351404675b6dddb725ec8bceb",
+    "url": "/static/media/code.4ee9ace3.jpg"
+  },
+  {
+    "revision": "be8e98ba7247c894ca2c98e9abd4b363",
+    "url": "/static/media/cv.be8e98ba.pdf"
+  },
+  {
+    "revision": "67b1cfbffda7e4369b3419c2513ea328",
+    "url": "/static/media/employee-tracker.67b1cfbf.png"
+  },
+  {
+    "revision": "f4ed775848f968f93007b8128b4f4919",
+    "url": "/static/media/profile-pic.f4ed7758.png"
+  },
+  {
+    "revision": "a747b3ee3662a345f198f3e3fbd7492a",
+    "url": "/static/media/readme-generator.a747b3ee.png"
+  },
+  {
+    "revision": "1cb1bfdf1a536ec950c7c74ed511512d",
+    "url": "/static/media/weather-dashboard.1cb1bfdf.png"
+  },
+  {
+    "revision": "58c7e9aaa293dad0d4284992866d6ac1",
+    "url": "/static/media/workout-tracker.58c7e9aa.png"
+  }
+]);
