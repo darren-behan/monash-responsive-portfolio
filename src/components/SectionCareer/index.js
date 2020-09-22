399fd1508d676d206c691255f02287091731ad7d
Darren Behan <darrenbehan@hotmail.com>
2020-09-22 10:25:09 +1000

    updated job description to render as list

--- src/components/SectionCareer/index.js
+++ src/components/SectionCareer/index.js
@@ -23,34 +23,46 @@ function Career() {
           aria-controls="collapse-one"
           aria-expanded={openOne}
         >
-          <h3 className="vertical-timeline-element-title" style={{ color: '#fff' }}>ERP Support Coordinator</h3>
+          <h4 className="vertical-timeline-element-title" style={{ color: '#fff' }}>ERP Support Coordinator</h4>
         </Button>
         <Collapse in={openOne}>
           <div id="collapse-one">
             <h5 className="vertical-timeline-element-subtitle">REA Group</h5>
             <p>
-              Developing Javascript scripts along with test
-              scripts using Jasmine to deliver business
-              requirements. 
-              Translating business requirements into clear,
-              concise, testable and accurate user stories &
-              process flows.
-              Ensuring that requirements are understood
-              through story kickoffs and constant
-              communication using a variety of analysis and
-              documentation techniques including the
-              application of visualization techniques to
-              underpin a joint understanding of processes and
-              data.
-              Coordinate Netsuite upgrades including,
-              development and execution of test cases to
-              ensure upgrades occur without any business
-              disruption.
-              Maintain/Update Netsuite/Adaptive account and
-              organization structures, workflows and processes
-              Supporting internal stakeholders as the main
-              contact for all requests and enhancements for
-              the companies ERP system (NetSuite).
+              <ul>
+                <li>
+                  Developing Javascript scripts along with test
+                  scripts using Jasmine to deliver business
+                  requirements.
+                </li>
+                <li>
+                  Translating business requirements into clear,
+                  concise, testable and accurate user stories &
+                  process flows.
+                </li>
+                <li>
+                  Ensuring that requirements are understood
+                  through story kickoffs and constant
+                  communication using a variety of analysis and
+                  documentation techniques including the
+                  application of visualization techniques to
+                  underpin a joint understanding of processes and
+                  data.
+                </li>
+                <li>
+                  Coordinate Netsuite upgrades including,
+                  development and execution of test cases to
+                  ensure upgrades occur without any business
+                  disruption.
+                </li>
+                <li>
+                  Maintain/Update Netsuite/Adaptive account and
+                  organization structures, workflows and processes
+                  Supporting internal stakeholders as the main
+                  contact for all requests and enhancements for
+                  the companies ERP system (NetSuite).
+                </li>
+              </ul>
             </p>
           </div>
         </Collapse>
@@ -65,24 +77,36 @@ function Career() {
           aria-controls="collapse-two"
           aria-expanded={openTwo}
         >
-          <h3 className="vertical-timeline-element-title">Accounts Payable Officer</h3>
+          <h4 className="vertical-timeline-element-title">Accounts Payable Officer</h4>
         </Button>
         <Collapse in={openTwo}>
           <div id="collapse-two">
             <h5 className="vertical-timeline-element-subtitle">REA Group</h5>
             <p>
-              Process all new & existing invoices requiring
-              payment by directing these to the relevant
-              requisitioners.
-              Performing weekly domestic and international pay
-              runs for an international organization.
-              Generating data for the processing of monthly
-              accruals throughout the organization.
-              Daily receipting tasks of all payments received.
-              General support for the accounts receivables
-              department.
-              Supporting the various vendors and internal
-              stakeholders on a daily basis.
+              <ul>
+                <li>
+                  Process all new & existing invoices requiring
+                  payment by directing these to the relevant
+                  requisitioners.
+                </li>
+                <li>
+                  Performing weekly domestic and international pay
+                  runs for an international organization.
+                </li>
+                <li>
+                  Generating data for the processing of monthly
+                  accruals throughout the organization.
+                </li>
+                <li>
+                  Daily receipting tasks of all payments received.
+                  General support for the accounts receivables
+                  department.
+                </li>
+                <li>
+                  Supporting the various vendors and internal
+                  stakeholders on a daily basis.
+                </li>
+              </ul>
             </p>
           </div>
         </Collapse>
@@ -97,35 +121,51 @@ function Career() {
           aria-controls="collapse-three"
           aria-expanded={openThree}
         >
-          <h3 className="vertical-timeline-element-title">Customer Credit Controller</h3>
+          <h4 className="vertical-timeline-element-title">Customer Credit Controller</h4>
         </Button>
         <Collapse in={openThree}>
           <div id="collapse-three">
             <h5 className="vertical-timeline-element-subtitle">REA Group</h5>
-            <p style={{ textAlign: 'right' }}>
-              Overseen a ledger of 3,000+ accounts.
-              Timely and effective collection of all debts and
-              customers payments.
-              Negotiating payment plans.
-              Resolving queries both internally and externally
-              around outstanding invoices.
-              Posting and allocating daily receipts.
-              Checking customer’s credit ratings with our
-              external partners.
-              Process Improvement by delivering a new CRM
-              (Zendesk) to increase our level of service with
-              our customers and provide reporting on customer
-              behaviours.
-              Weekly Project catch ups and reporting to ensure
-              implemented changes are successfully impacting
-              our customers and internal stakeholders.
-              Communication with internal & external
-              stakeholders for process changes, requirements
-              for further development and any assistance
-              required within the credit process.
-              Process and user journey mapping to enable
-              further discovery for process improvement and
-              automation.
+            <p>
+              <ul>
+                <li>
+                  Overseen a ledger of 3,000+ accounts.
+                  Timely and effective collection of all debts and
+                  customers payments.
+                </li>
+                <li>
+                  Negotiating payment plans.
+                  Resolving queries both internally and externally
+                  around outstanding invoices.
+                </li>
+                <li>
+                  Posting and allocating daily receipts.
+                  Checking customer’s credit ratings with our
+                  external partners.
+                </li>
+                <li>
+                  Process Improvement by delivering a new CRM
+                  (Zendesk) to increase our level of service with
+                  our customers and provide reporting on customer
+                  behaviours.
+                </li>
+                <li>
+                  Weekly Project catch ups and reporting to ensure
+                  implemented changes are successfully impacting
+                  our customers and internal stakeholders.
+                </li>
+                <li>
+                  Communication with internal & external
+                  stakeholders for process changes, requirements
+                  for further development and any assistance
+                  required within the credit process.
+                </li>
+                <li>
+                  Process and user journey mapping to enable
+                  further discovery for process improvement and
+                  automation.
+                </li>
+              </ul>
             </p>
           </div>
         </Collapse>
@@ -140,27 +180,45 @@ function Career() {
           aria-controls="collapse-four"
           aria-expanded={openFour}
         >
-          <h3 className="vertical-timeline-element-title">Customer Experience Consultant</h3>
+          <h4 className="vertical-timeline-element-title">Customer Experience Consultant</h4>
         </Button>
         <Collapse in={openFour}>
           <div id="collapse-four">
-            <h5 className="vertical-timeline-element-subtitle">REA Groups</h5>
+            <h5 className="vertical-timeline-element-subtitle">REA Group</h5>
             <p>
-              Assisting in the development of current
-              processes like implementing Zendesk and a
-              Salesforce CRM.
-              Process and User Journey Mapping.
-              Internal stakeholder interviews translated into
-              data and further workshopping.
-              Communicate findings to wider team and
-              management.
-              Knowledge Sharing across Digital Marketing,
-              Products, Programming, Accounts.
-              Maintaining 48 hour SLA's.
-              Delivering high levels of customer service to
-              internal and external stakeholders.
-              Day-to-day tasks such as Resourcing & assisting
-              all queues.
+              <ul>
+                <li>
+                  Assisting in the development of current
+                  processes like implementing Zendesk and a
+                  Salesforce CRM.
+                </li>
+                <li>
+                  Process and User Journey Mapping.
+                </li>
+                <li>
+                  Internal stakeholder interviews translated into
+                  data and further workshopping.
+                </li>
+                <li>
+                  Communicate findings to wider team and
+                  management.
+                </li>
+                <li>
+                  Knowledge Sharing across Digital Marketing,
+                  Products, Programming, Accounts.
+                </li>
+                <li>
+                  Maintaining 48 hour SLA's.
+                </li>
+                <li>
+                  Delivering high levels of customer service to
+                  internal and external stakeholders.
+                </li>
+                <li>
+                  Day-to-day tasks such as Resourcing & assisting
+                  all queues.
+                </li>
+              </ul>
             </p>
           </div>
         </Collapse>
